d57208289f78a3e32d745d4408a3f2f3473e7d32
James F. Leist <james.leist.13@cnu.edu>
2017-03-12 19:07:44 -0400

    Goalie and Player

--- app/src/main/java/com/example/james/soccer/Goalie.java
+++ app/src/main/java/com/example/james/soccer/Goalie.java
@@ -1,5 +1,7 @@
 package com.example.james.soccer;
 
+import android.graphics.Point;
+
 /**
  * Created by james on 3/7/2017.
  */
@@ -11,18 +13,29 @@ public class Goalie extends Player {
 
     @Override
     public void setZone(int SIDE_OF_FIELD) {
-
+        if(SIDE_OF_FIELD == Constants.RIGHT_SIDE){
+            setZoneCenter(new Point(Constants.G_X, Constants.G_Y_START));
+        } else {
+            setZoneCenter(new Point(-Constants.G_X, Constants.G_Y_START));
+        }
     }
 
     @Override
     public void defend(Ball ball) {
-        if(ball.getLocation().y>15){
-            
+        if(ball.getLocation().y > 15){
+            setLocation(new Point(getLocation().x, 15));
+        } else if(ball.getLocation().y < -15){
+            setLocation(new Point(getLocation().x, -15));
+        } else {
+            setLocation(new Point(getLocation().x, ball.getLocation().y));
+        }
+        if(ball.getDistanceFrom(getLocation())<=(15*(getGoalkeeping()/100))){
+            this.move(ball.getLocation());
         }
     }
 
     @Override
     public void attack(Ball ball) {
-
+        this.move(getZoneCenter());
     }
 }
