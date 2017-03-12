f92ae528e472490bb2ee7d9ef4625872f20455c3
Richard Frolia <richard.frolia.13@cnu.edu>
2017-03-12 18:52:32 -0400

    GamePlay Activity Change

--- app/src/main/java/com/example/james/soccer/GameplayActivity.java
+++ app/src/main/java/com/example/james/soccer/GameplayActivity.java
@@ -1,7 +1,8 @@
 package com.example.james.soccer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 
 public class GameplayActivity extends AppCompatActivity {
 
@@ -10,4 +11,30 @@ public class GameplayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameplay);
     }
+    Striker st = new Striker("Air", "Bud", "RS", new int[]{26, 99, 99, 99, 99, 99, 99, 99, 99, 0, 1});
+    Goalie gk = new Goalie("Manuel","Neuer", new int[]{76,93,84,80,68,78,80,64,80,93,2});
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int)event.getX();
+        int y = (int)event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                createMove();
+            case MotionEvent.ACTION_MOVE:
+                createPass();
+            case MotionEvent.ACTION_UP:
+        }
+        return false;
+    }
+
+    public void createPass(){
+
+    }
+
+    public void createMove(){
+
+    }
+
+
 }
