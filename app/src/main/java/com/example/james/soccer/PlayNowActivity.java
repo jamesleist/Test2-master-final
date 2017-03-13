5ce68471a30fa43fc885c45cdb135d582f13fd0f
Richard Frolia <richard.frolia.13@cnu.edu>
2017-03-12 20:16:57 -0400

    Practice Activity

--- app/src/main/java/com/example/james/soccer/PlayNowActivity.java
+++ app/src/main/java/com/example/james/soccer/PlayNowActivity.java
@@ -1,33 +1,13 @@
 package com.example.james.soccer;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.os.Bundle;
 
-public class PlayNowActivity extends AppCompatActivity{
-    private int TEAM_SELECT_CODE = 1;
+public class PlayNowActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_now);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    public void teamSelect(View view) {
-        Intent myIntent = new Intent(this,TeamSelectActivity.class);
-        startActivityForResult(myIntent, TEAM_SELECT_CODE);
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == TEAM_SELECT_CODE) {
-            if(resultCode == RESULT_OK) {
-
-            }
-        }
     }
 }
--- app/src/main/java/com/example/james/soccer/PracticeActivity.java
+++ app/src/main/java/com/example/james/soccer/PracticeActivity.java
@@ -1,23 +1,13 @@
 package com.example.james.soccer;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by Richard Frolia on 3/12/2017.
- */
+import android.os.Bundle;
 
 public class PracticeActivity extends AppCompatActivity {
-    Striker st = new Striker("Air", "Bud", "RS", new int[]{26, 99, 99, 99, 99, 99, 99, 99, 99, 0, 1});
-    Goalie gk = new Goalie("Donald", "Trump", new int[]{70, 85, 85, 85, 85, 85, 85, 85, 85, 85, 12});
-
-
-    GameplayActivity g = new GameplayActivity();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
-
     }
 }
